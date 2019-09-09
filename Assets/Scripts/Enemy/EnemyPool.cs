89213f646e271cfd2d252da5a88c0510fc099438
RMIT\s3646160 <s3646160@student.rmit.edu.au>
2019-09-09 14:14:59 +1000

    Fixed the project

---
+++ Assets/Scripts/Enemy/EnemyPool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+	public List<Transform> spawningSpots;
+	public GameObject enemyPrefab;
+	private List<GameObject> enemies = new List<GameObject>();
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void PullOneEnemy()
+	{
+		Transform spot = spawningSpots[Random.Range(0, spawningSpots.Count)];
+		foreach (GameObject survivor in enemies)
+		{
+			if (!survivor.activeSelf)
+			{
+				survivor.transform.SetParent(spot);
+				survivor.transform.position = spot.position;
+				survivor.SetActive(true);
+				return;
+			}
+		}
+		GameObject newEnemy = Instantiate(enemyPrefab, spot.position, spot.rotation, transform);
+		enemies.Add(newEnemy);
+	}
+
+	private Vector2 GetRandomForce()
+	{
+		return new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized * 10f;
+	}
+}
