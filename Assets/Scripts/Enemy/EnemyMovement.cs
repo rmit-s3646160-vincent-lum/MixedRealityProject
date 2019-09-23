97abb22d7980c05e6875e6b1fab6932c9576e45c
Billiams98 <bill.williams20210@gmail.com>
2019-09-24 01:38:04 +1000

    Revert "Merging of Branches"
    This reverts commit 53093e55e84bbee999a394943e3c1f9ac2364091, reversing
    changes made to 407d3ab21dd1bd1e20dd35944a6783f843bf90cf.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+	private Rigidbody rigidbody;
+
+	public float maxSpeed = 5f;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+		FaceForward();
+		if (rigidbody.velocity.magnitude > maxSpeed)
+		{
+			rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+		}
+	}
+
+	private void FaceForward()
+	{
+		if (rigidbody.velocity != Vector3.zero)
+		{
+			float angle = rigidbody.velocity.z < 0 ?
+				Vector3.Angle(new Vector3(1, 0, 0), rigidbody.velocity.normalized)
+				: -Vector3.Angle(new Vector3(1, 0, 0), rigidbody.velocity.normalized);
+			transform.rotation = Quaternion.Euler(0, angle, 0);
+		}
+	}
+
+	public void StopMove()
+	{
+		rigidbody.velocity = Vector3.zero;
+	}
+
+	public void ApplyForce(Vector3 force)
+	{
+		rigidbody.AddForce(force, ForceMode.Impulse);
+	}
+
+}
