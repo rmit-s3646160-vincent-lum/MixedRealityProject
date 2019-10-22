66d2baacb1fa74d4498c52dee8992cfa3c7a1e3c
Vincent Lum <s3646160@student.rmit.edu.au>
2019-10-23 00:23:54 +1100

    Bell and Traffic light interactions added

---
+++ Assets/Scripts/Scrap/TrafficLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    [SerializeField] GameObject redLight, greenLight;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greenLight.SetActive(false);
+    }
+
+    public void SwitchLights()
+    {
+        redLight.SetActive(!redLight.activeSelf);
+        greenLight.SetActive(!greenLight.activeSelf);
+    }
+}
